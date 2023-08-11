065903131ae89c333771030e8eff5a518e4d8363
Cristian Lopez <ccristian1262@gmail.com>
2023-08-10 21:35:14 -0300

    no encuentro el error

--- quienes-somos.js
+++ quienes-somos.js
@@ -1,9 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+//const getEmpleados= async()=>{
+//    const respuesta= await fetch("/quienes-somos.json")
+//    return await respuesta.json()
+//}
 
-const getEmpleados= async()=>{
-    const respuesta= await fetch(`/quienes-somos.json`)
-    return await respuesta.json()
-}
+//const empleados= getEmpleados()
+//console.log(empleados)
+//const cargarJsonArray= document.querySelector(`#cargarJsonArray`);
 
-const empleados= getEmpleados()
-console.log(empleados)
-const cargarJsonArray= document.querySelector(`#cargarJsonArray`);
+
+const getEmpleados = async () => {
+	const respuesta = await fetch(`/quienes-somos.json`);
+	let empleados = await respuesta.json();
+	console.log(empleados);
+};
+
+getEmpleados();
+
+
+
+});
